51ba5f49b45edf1937429bef952c04cee11eafa6
Slobodan Dolinic <slobodan@digitalcube.rs>
2017-11-11 13:12:39 +0100

    fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api_url: '',
-  ws_url: `ws://${window.location.hostname}:8999/ws`,    // websocket url
+  ws_url: `ws://${window.location.hostname}:8991/ws`,    // websocket url
   stop_web_socket: false,
 };
